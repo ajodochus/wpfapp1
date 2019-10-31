9b34c76c448f4f210feb31db6347d119339b2b0d
bernd.wissmeier <bernd.wissmeier@stp-online.de>
2019-10-31 22:24:12 +0100

    change on master

---
+++ WpfApp1/Class1.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace WpfApp1
+{
+    class Class1
+    {
+        // master
+    }
+}
